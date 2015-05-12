1582946c0518edc818c76304bf305a9142ca40fe
Aditya Shingvi <adityashingvi13@gmail.com>
2015-05-12 12:53:08 -0700

    Added Aspect for BookController for fetching books

--- src/edu/sjsu/cmpe275/lab3/AOPFileAccessAspect.java
+++ src/edu/sjsu/cmpe275/lab3/AOPFileAccessAspect.java
@@ -2,6 +2,7 @@ package edu.sjsu.cmpe275.lab3;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -11,9 +12,13 @@ import org.springframework.aop.aspectj.annotation.*;
 @Aspect
 public class AOPFileAccessAspect {
 
-	@Before("execution(* edu.sjsu.cmpe275.lab3.BookController.getBooks(..))")
-	public void validateShare() throws Throwable {
-		System.out.println("Before AOP");
+	@Before("execution(* edu.sjsu.cmpe275.lab3.BookController.getBooks(*))")
+	public void validateShare(JoinPoint join) throws Throwable {
+		System.out.println("Before AOP - " + join.getArgs().toString());
+		Object[] objects = join.getArgs();
+		HttpServletRequest request = (HttpServletRequest)objects[0];
+		long uid = (Long) request.getSession().getAttribute("userid");
+		System.out.println(uid);
 	//	jp.proceed();
 		System.out.println("After AOP");
 	}
